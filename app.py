31a5f0092670ab83122309043f26ceca9a9575b6
[Gustavo Rios] <gustavorioslife@gmail.com>
2024-12-12 10:43:36 -0700

    map layout created

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 # Flask Entry point
 from flask import Flask, render_template, request, jsonify
+from dotenv import load_dotenv
+import os
+
+# load .env variable
+load_dotenv()
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,6 +15,15 @@ def home():
 @app.route('/api/data', methods=['GET'])
 def get_data():
     return jsonify({'message': 'Hello, world!'})
+
+@app.route('/getGoogleMapsKey')
+def get_google_maps_key():
+    api_key = os.getenv('GOOGLE_MAPS_API_KEY')
+    return jsonify({'apiKey': api_key})
+
+@app.route('/map')
+def mapView():
+    return render_template('/map.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
